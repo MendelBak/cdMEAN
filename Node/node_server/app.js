e7f806e57b86e3083d1efd1c57da38944e5615ab
Mendel <mendelbak@gmail.com>
2017-11-28 15:21:53 -0600

    JS and Node assignments

---
+++ Node/node_server/app.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(request, response){
+    console.log('client request URL:', request.url);
+
+    if(request.url === '/') {
+        fs.readFile('templates/index.html', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/torah') {
+        fs.readFile('templates/torah.html', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === "/stylesheets/style.css") {
+        fs.readFile("stylesheets/style.css", "utf8", function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(contents);
+            response.end();
+        });
+    }
+
+    else{
+        response.writeHead(404);
+        response.end('File not found! Now, go fly a kite, elsewhere.');
+    }
+});
+server.listen(613);
+console.log("Running in locahost at port 613")
